78899a0a8257f5461b23ec38d3759957bb2ecf77
Talha Çöplü <talhacoplu@gmail.com>
2022-09-23 23:37:44 +0300

    sfdg

--- src/Day001Github.java
+++ src/Day001Github.java
@@ -4,7 +4,7 @@ public class Day001Github {
 
         System.out.println("Git deneme");
         System.out.println("bi daha deneme");
-        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aasdfsdfsdfsdfsdfsdfsdffaaaa");
         System.out.println("aaafffffffaaaa");
     }
 }
