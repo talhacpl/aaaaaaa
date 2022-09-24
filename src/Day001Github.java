e3569dc5bde10bdc4a2486610891b5b3fff0c65e
Talha Çöplü <talhacoplu@gmail.com>
2022-09-24 18:22:27 +0300

    SÜPER BİR DENEME

--- src/Day001Github.java
+++ src/Day001Github.java
@@ -3,12 +3,8 @@ public class Day001Github {
     public static void main(String[] args) {
 
         System.out.println("Git deneme");
-        System.out.println("bi daha deneme");
-        System.out.println("aasdfsdfsdfsdfsdfsdfsdffaaaa");
-        System.out.println("aaafffffffaaaa");
-        System.out.println("aaafffffffaaaa");
-        System.out.println("aaafffffffaaaa");
-        System.out.println("aaafffffffaaaa");
-        System.out.println("aaafffffffaaaa");
+        System.out.println("aa0000000000000da");
+        System.out.println("aa000000000000fsda");
+        System.out.println("a00000000000");
     }
 }
