092eedc159c9aa459531348f67cbb6d2d8385020
Talha Çöplü <talhacoplu@gmail.com>
2022-09-24 22:42:54 +0300

    VVVV

--- src/Day001Github.java
+++ src/Day001Github.java
@@ -3,15 +3,12 @@ public class Day001Github {
     public static void main(String[] args) {
 
         System.out.println("Git deneme");
-        System.out.println("aa0000000000000da");
-        System.out.println("aa000000000000fsda");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
-        System.out.println("a00000000000");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
+        System.out.println("Git deneme");
     }
 }
