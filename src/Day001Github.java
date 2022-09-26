c14ec445d75c40607b9471b82cca345d395eb41e
Talha Çöplü <talhacoplu@gmail.com>
2022-09-26 15:14:55 +0300

    K9

--- src/Day001Github.java
+++ src/Day001Github.java
@@ -1,24 +1,8 @@
 public class Day001Github {
 
     public static void main(String[] args) {
-
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
-        System.out.println("Git deneme");
         System.out.println("TALHA");
+        // gfdgdfgdfgdfg
+        //dfgdfgdfg
     }
 }
